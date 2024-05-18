740d0725eb76f7eed8a836ef10656420ad70ebd1
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2024-05-18 07:44:29 +0200

    Update Home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   useEffect(()=> {
     setLoading(true)
     axios
-        .get('https://mern-stack-deploy-server.vercel.app/book')
+        .get('https://mern-backend-jy18.onrender.com/book')
             .then((response)=> {
                 setLoading(false)
                 setBooks(response.data.data)
@@ -33,7 +33,7 @@ const Home = () => {
                         <p>{book.book_name}</p>
                         <p>{book.author}</p>
                         <p>{book.description}</p>
-                        <img src={`https://mern-stack-deploy-server.vercel.app/${book.image}`} className='w-[150px]' alt="Image" />
+                        <img src={`https://mern-backend-jy18.onrender.com/${book.image}`} className='w-[150px]' alt="Image" />
                         <Link to={`/delete/${book._id}`}>Delete</Link>
                         <Link to={`/update/${book._id}`}>Update</Link>
                     </div>
