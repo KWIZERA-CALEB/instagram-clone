d8f025ae88773eb8382b8e956e15e7e63798f325
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2025-01-01 10:44:26 +0100

    added games

---
+++ Frontend/src/components/DoodleJump.jsx
@@ -0,0 +1,13 @@
+
+const DoodleJumpGame = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Play Doodle Jump!</h1>
+            <a href="/games/doodlejump.html" target="_blank" rel="noopener noreferrer">
+                Open Game Tab
+            </a>
+        </div>
+    );
+};
+
+export default DoodleJumpGame;
